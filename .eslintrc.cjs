712f5535ce2965a282e3c005b77ec33d268d40bb
EM <1351468279@qq.com>
2023-06-18 14:52:49 +0800

    第三次了

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        printWidth: 80,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index']
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'],
+    // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
+    'no-undef': 'error'
+  }
+  /* 格式：单引号，没有分号，行宽度80字符，
+  没有对象数组最后一个逗号，换行字符串自动（系统不
+  一样换行符号不一样） */
+}
